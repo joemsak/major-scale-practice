e04e0b279d2a6be55adb594cb899d17514b8865b
Joe Sak <joe@joesak.com>
2024-03-05 18:14:37 -0700

    WIP getting ready for the chord tone algorithm

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,153 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import cx from "clsx";
 
 export default function Home() {
+  const [selectedRoot, setSelectedRoot] = useState("");
+  const [selectedScales, setSelectedScales] = useState([]);
+
+  const notes = [
+    "C",
+    "Db",
+    "D",
+    "Eb",
+    "E",
+    "F",
+    "Gb",
+    "G",
+    "Ab",
+    "A",
+    "Bb",
+    "B",
+  ];
+
+  const scales = [
+    {
+      numeral: "I",
+      steps: 2,
+      type: "major",
+      isMajor: true,
+    },
+    {
+      numeral: "ii",
+      steps: 2,
+      type: "minor",
+      isMajor: false,
+    },
+    {
+      numeral: "iii",
+      steps: 1,
+      type: "minor",
+      isMajor: false,
+    },
+    {
+      numeral: "IV",
+      steps: 2,
+      type: "major",
+      isMajor: true,
+    },
+    {
+      numeral: "V",
+      steps: 2,
+      type: "dominant",
+      isMajor: true,
+    },
+    {
+      numeral: "vii",
+      steps: 2,
+      type: "minor",
+      isMajor: false,
+    },
+    {
+      numeral: "vii°",
+      steps: 1,
+      type: "half-diminished",
+      isMajor: false,
+    },
+  ];
+
+  const selectedScaleColors = [
+    "bg-green-600",
+    "bg-sky-600",
+    "bg-pink-600",
+    "bg-orange-600",
+  ];
+
+  const chordTones = (scale) => {
+    return [];
+  };
+
+  const selectedRootStyle = (note) =>
+    selectedRoot == note
+      ? "bg-slate-100 text-black pointer-events-none"
+      : "bg-slate-600 text-slate-400 hover:bg-slate-300";
+
+  const selectedScaleStyle = (scale) => {
+    const idx = selectedScales.map((s) => s.numeral).indexOf(scale.numeral);
+
+    if (idx > -1) {
+      return cx("text-white", selectedScaleColors[idx]);
+    } else if (selectedScales.length < 4) {
+      return "bg-slate-600 text-slate-400 hover:bg-slate-300";
+    } else {
+      return "opacity-70 bg-slate-600 text-slate-400 pointer-events-none";
+    }
+  };
+
+  const manageSelectedScales = (scale) => {
+    if (selectedScales.includes(scale))
+      setSelectedScales([...selectedScales.filter((s) => s !== scale)]);
+    else if (selectedScales.length < 4)
+      setSelectedScales([...selectedScales, scale]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="py-8 px-24">
+      <div className="flex justify-between">
+        {notes.map((note, i) => (
+          <button
+            key={`root-picker-${i}`}
+            className={cx(
+              "mr-4 last:mr-0 text-6xl py-2 rounded-lg w-[115px]",
+              selectedRootStyle(note)
+            )}
+            onClick={(e) => setSelectedRoot(e.target.textContent)}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {note}
+          </button>
+        ))}
+      </div>
+
+      <div className="mt-8 flex justify-between">
+        {scales.map((scale, i) => (
+          <button
+            key={`scale-picker-${i}`}
+            className={cx(
+              "mr-4 last:mr-0 text-6xl py-2 rounded-lg w-[115px]",
+              selectedScaleStyle(scale)
+            )}
+            onClick={() => manageSelectedScales(scale)}
+          >
+            {scale.numeral}
+          </button>
+        ))}
+      </div>
+
+      {selectedScales.map((scale, i) => (
+        <div key={`selected-scale-${i}`} className="mt-8">
+          {chordTones(scale).map((tone, n) => (
+            <div
+              key={`tone-${i}-${n}`}
+              className={cx(
+                "mr-4 last:mr-0 text-6xl py-2 rounded-lg w-[115px] text-center",
+                selectedScaleStyle(scale)
+              )}
+            >
+              {tone}
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
   );
 }
